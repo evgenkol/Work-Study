36b6a215566ef45b4c2f1a8b42b3f99e3136cec6
Yevhen Kolobaiev <koevgenkol2gmail.com>
2022-05-21 21:24:28 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,1 +1,11 @@
-
+let message = "Hello!";
+let number = 2;
+if (2 > 1) {
+  console.log(message);
+}
+message = "Hello Zosya!";
+if (number > 1) {
+  console.log(message);
+} else {
+  console.log("((((");
+}
