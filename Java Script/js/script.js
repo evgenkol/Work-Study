36a584a53c4124d22496e78996a84f97e3c4b39b
Yevhen <kolevgenkol@gmail.com>
2022-05-30 21:46:35 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -43,16 +43,78 @@
 //   console.log(num);
 // }
 
-function calcsum(a = 2, b = 3) {
-  let sum = a + b;
-  let prod = a * b;
-  let remainder = prod + sum;
-  console.log(sum);
-  console.log(prod);
-  console.log(`Сумма функций ${remainder}`);
-  return remainder;
-  if (condition) {
-  }
+// function calcsum(a = 2, b = 3) {
+//   let sum = a + b;
+//   let prod = a * b;
+//   let remainder = prod + sum;
+//   console.log(sum);
+//   console.log(prod);
+//   console.log(`Сумма функций ${remainder}`);
+//   // if (remainder > 20) {
+//   //   alert("Мало))");
+//   // }
+//   return remainder;
+// }
+// calcsum(6, 7);
+// calcsum();
+// let funcRezult = calcsum(3, 4);
+// console.log(`Сумма ${funcRezult}`);
+
+// function calcsum(numOne, numTwo, more, less) {
+//   let numSum = numOne + numTwo;
+//   if (numSum > 3) {
+//     more();
+//   } else {
+//     less();
+//   }
+// }
+
+// function showMoreMassage() {
+//   console.log("Больше чем 3");
+// }
+// function showLessMassage() {
+//   console.log("Меньше чем 3");
+// }
+// calcsum(1, 1, showMoreMassage, showLessMassage);
+
+// function calcSum(numOne, numTwo) {
+//   if (numTwo === 1) {
+//     return numOne;
+//   } else {
+//     return numOne * calcSum(numOne, numTwo - 1);
+//   }
+// }
+// console.log(calcSum(2, 5));
+
+// function calcSum(numOne, numTwo) {
+//   return numTwo === 1 ? numOne : numOne * calcSum(numOne, numTwo - 1);
+// }
+// console.log(calcSum(2, 7));
+
+// function pow(x, n) {
+//   return n == 1 ? x : x * pow(x, n - 1);
+// }
+
+// console.log(pow(2, 3));
+// ----------------------------------------------------------------------------------
+// let getMessage = (text, name) => {
+//   let message = text + ", " + name + "!";
+//   return message; //!!!!!
+// };
+// console.log(getMessage("Hello", "Bet"));
+
+// let getMessage1 = (text, name) => text + ", " + name + "!";
+
+// console.log(getMessage1("Hello", "Mike"));
+// -----------------------------------------------------------------------------
+
+function showMessage(text, name) {
+  console.log(`${text}, ${name}!`);
 }
-calcsum(6, 7);
-calcsum();
+setTimeout(showMessage, 3000, "Hello", "Dolly");
+setTimeout(showMessage, 3000, "Hello", "Dolly");
+
+// function showMessage(text, name) {
+//   console.log(`${text}, ${name}!`);
+// }
+// setInterval(showMessage, 3000, "Hello", "Dolly");
