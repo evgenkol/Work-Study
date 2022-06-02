277e205d3621715e315fbeef5888d29cb416afae
Yevhen <kolevgenkol@gmail.com>
2022-06-02 16:08:47 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,11 +1,7 @@
-let message = "Hello!";
-let number = 2;
-if (2 > 1) {
-  console.log(message);
-}
-message = "Hello Zosya!";
-if (number > 1) {
-  console.log(message);
-} else {
-  console.log("((((");
-}
+let userName = {
+  name: "Yevhen",
+  age: 39,
+};
+
+console.log(userName.age);
+console.log(userName);
