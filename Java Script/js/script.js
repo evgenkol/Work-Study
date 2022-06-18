711da0c808dfed6e35df773873f54107d80aea71
Yevhen <kolevgenkol@gmail.com>
2022-06-18 21:27:27 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,3 +1,22 @@
-let repeatStr = (n, s) => s.repeat(n);
+let humanYearsCatYearsDogYears = function (humanYears) {
+  console.log(humanYears);
+  let catYears =
+    humanYears == 1
+      ? humanYears + 15 - humanYears
+      : humanYears == 2
+      ? humanYears + 15 + 9 - humanYears
+      : (humanYears - 2) * 4 + 24;
+  let dogYears =
+    humanYears == 1
+      ? humanYears + 15 - humanYears
+      : humanYears == 2
+      ? humanYears + 15 + 9 - humanYears
+      : (humanYears - 2) * 5 + 24;
 
-console.log(repeatStr(2, "ha "));
+  return [humanYears, catYears, dogYears];
+};
+
+console.log(humanYearsCatYearsDogYears(1));
+console.log(humanYearsCatYearsDogYears(2));
+console.log(humanYearsCatYearsDogYears(10));
+console.log(humanYearsCatYearsDogYears(20));
