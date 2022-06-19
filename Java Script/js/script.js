499d463c04def435734db2ee5b4b2f3e92c253a5
Yevhen <kolevgenkol@gmail.com>
2022-06-19 18:15:30 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,13 +1,3 @@
-// function betterThanAverage(classPoints, yourPoints) {
-//   let classAverage = classPoints.reduce((a, b) => a + b, 0);
-//   console.log(classAverage);
-
-//   yourPoints > classAverage ? true : false;
-// }
-
-let betterThanAverage = (classPoints, yourPoints) =>
-  yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
-
-// console.log(yourPoints);
-// console.log(classPoints);
-console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 88));
+function getAge(inputString) {
+  return parseInt(inputString);
+}
