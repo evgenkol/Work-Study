5487030159ce6950cecbd14a599f41bce0190cbb
Yevhen <kolevgenkol@gmail.com>
2022-06-20 17:48:40 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,3 +1,23 @@
-function getAge(inputString) {
-  return parseInt(inputString);
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+  let mix = birds.filter((x) => !geese.includes(x));
+  return mix;
 }
+
+// let gooseFilter = (a, b) => b.filter((x) => a.includes(x));
+
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ])
+);
+console.log(
+  gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])
+);
