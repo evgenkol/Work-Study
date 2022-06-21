e9ca0bf67bd4ca89a940555493527f06bff2c327
Yevhen <kolevgenkol@gmail.com>
2022-06-21 11:53:17 +0300

    MASTER

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,23 +1,3 @@
-function gooseFilter(birds) {
-  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+let remove = (a) => a.replace(/!$/, "");
 
-  let mix = birds.filter((x) => !geese.includes(x));
-  return mix;
-}
-
-// let gooseFilter = (a, b) => b.filter((x) => a.includes(x));
-
-console.log(
-  gooseFilter([
-    "Mallard",
-    "Hook Bill",
-    "African",
-    "Crested",
-    "Pilgrim",
-    "Toulouse",
-    "Blue Swedish",
-  ])
-);
-console.log(
-  gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])
-);
+console.log(remove("!Hi!!!"));
