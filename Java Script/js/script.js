97697ee75bda774216f3a1e2a902c6fd2722f01d
Yevhen <kolevgenkol@gmail.com>
2022-06-22 23:20:49 +0300

    master

--- Java Script/js/script.js
+++ Java Script/js/script.js
@@ -1,12 +1,3 @@
-let well = (x) =>
-  x.filter((a) => a == "good").length == 0
-    ? "Fail!"
-    : x.filter((a) => a == "good").length <= 2
-    ? "Publish!"
-    : "I smell a series!";
+let a = (a, b) => a.filter((x) => !b.includes(x));
 
-console.log(
-  well(["good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"])
-);
-console.log(well(["bad", "bad", "bad"]));
-console.log(well(["good", "bad", "bad", "bad", "bad"]));
+console.log(a([5], [12]));
